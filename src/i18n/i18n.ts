2533d0b79ddf13e53248a7dacd605ba764918004
King Wang <me@k8w.io>
2021-05-25 22:21:39 +0800

    i18n

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,6 +2,6 @@ import { i18n as zhCN } from './zh-cn';
 import { i18n as enUS } from './en-us';
 
 // 根据系统语言判断中英文
-// export const i18n = require('os-locale').sync() === 'zh-CN' ? zhCN : enUS;
+export const i18n = require('os-locale').sync() === 'zh-CN' ? zhCN : enUS;
 // TODO en-us
-export const i18n = zhCN;
+// export const i18n = zhCN;
